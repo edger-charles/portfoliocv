f37c51b2fd2686a0708f9af885215a1409aa4d92
edger-charles <148742265+edger-charles@users.noreply.github.com>
2024-10-25 09:09:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -161,29 +161,3 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initial check
     animateTextBoxes();
 });
-
-
-
-
-// Parallex ///////////////////////////////////////////////
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add("show-items");
-        }else{
-            entry.target.classList.remove("show-items")
-        }
-    })
-})
-
-
-
-
-const scrollScale = document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el));
-
-const scrollBottom = document.querySelectorAll(".scroll-Bottom");
-scrollBottom.forEach((el)=>observer.observe(el));
-
-const scrollTop = document.querySelectorAll(".scroll-Top");
-scrollTop.forEach((el)=>observer.observe(el));
